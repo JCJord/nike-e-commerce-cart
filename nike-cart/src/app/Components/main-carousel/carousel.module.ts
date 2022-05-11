7ca1c8a9475bcd942241f89b1003066c0f996513
Júlio César <juliojc.jord@gmail.com>
2022-05-11 10:53:50 -0300

    chore: Moduling carousel component & updating global scss import

---
+++ nike-cart/src/app/Components/main-carousel/carousel.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainCarouselComponent } from './main-carousel.component';
+import { CarouselComponent } from './carousel/carousel.component'
+
+
+@NgModule({
+  declarations: [MainCarouselComponent, CarouselComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CarouselModule { }
