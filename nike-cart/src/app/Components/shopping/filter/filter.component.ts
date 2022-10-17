014fd8abb0b434759282311c8320df81a322187a
Júlio César <julinnn4k@live.com>
2022-10-17 11:08:30 -0300

    refactor: imporvement on dropdrown

--- nike-cart/src/app/Components/shopping/filter/filter.component.ts
+++ nike-cart/src/app/Components/shopping/filter/filter.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Shoes } from 'src/app/Components/shopping/shoes.model';
 import { ShoppingListService } from 'src/app/Services/shopping-list-service';
@@ -5,12 +6,28 @@ import { ShoppingListService } from 'src/app/Services/shopping-list-service';
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        overflow: 'hidden',
+        height: '*',
+      })),
+      state('closed', style({
+        overflow: 'hidden',
+        height: '0px',
+      })),
+      transition('closed => open', animate(`250ms ease-in-out`)),
+      transition('open => closed', animate(`250ms ease-in-out`)),
+    ]),
+  ],
 })
 export class FilterComponent {
-  selectedFilter = 'Menor Preço'
+  selectedFilter = 'Maior Preço'
   activeGrid!: number;
   filterIsActive = true;
+  isOpen!: boolean;
+
   @Input()
   shoes!: Shoes[];
 
